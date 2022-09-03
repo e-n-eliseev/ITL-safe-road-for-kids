997174e3618e84197c5386541587eb1a9f073662
e-n-eliseev <e.n.eliseev@mail.ru>
2022-09-03 16:36:41 +0300

    smth

--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -3,7 +3,7 @@ import ReactPlayer from "react-player";
 const Video = () => {
     return (
         <section className="video">
-            <h2 className="video__heading"> Пожалуйста, посмотрите видео ниже, для закрепления основных правил поведения на дороге. </h2>
+            <h2 className="video__heading"> Пожалуйста, посмотрите видео ниже для закрепления основных правил поведения на дороге. </h2>
             <div className="video__url">
                 <ReactPlayer
                     url="https://www.youtube.com/watch?v=klxderXnVPc"
