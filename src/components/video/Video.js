fd173954cb73feb8e2d99405259f59c44fc08a9d
e-n-eliseev <e.n.eliseev@mail.ru>
2022-09-03 20:53:15 +0300

    stoplight deleted

--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -6,7 +6,7 @@ const Video = () => {
             <h2 className="video__heading"> Пожалуйста, посмотрите видео ниже для закрепления основных правил поведения на дороге. </h2>
             <div className="video__url">
                 <ReactPlayer
-                    url="https://www.youtube.com/watch?v=klxderXnVPc"
+                    url='https://www.youtube.com/watch?v=klxderXnVPc'
                 />
             </div >
         </section>
